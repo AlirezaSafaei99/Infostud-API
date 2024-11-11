f52e5f1269cfab3438c11f6574bea00cf5da18fe
AlirezaSafaei99 <alireza.s.f62@gmail.com>
2024-11-11 17:24:50 +0100

    Modifying yaml file, Defining DB models in models file, Defining CRUD Operatons in crud file

--- app/models.py
+++ app/models.py
@@ -1,1 +1,16 @@
 # Database models (User model)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
+    __table_args__ = {"schema": "users"}
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    info_users = Column(String(100))
+    info_password = Column(String(100))
+    
