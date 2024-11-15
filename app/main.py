9c399cca5de9bb5e3316125edc30dd0740c6b098
aradmehr@wheretech.it <aradmehr@wheretech.it>
2024-11-15 19:08:49 +0100

    Remove scheduler functionality and related imports from main application

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@
 
 from fastapi import FastAPI
 from app.routes import user
-from app.utils.scheduler import scheduler
 
 # Create a single fastAPI application instance
 app = FastAPI()
@@ -13,7 +12,4 @@ app = FastAPI()
 # Include the user-related router
 app.include_router(user.router)
 
-# Start the scheduler on the app startup
-@app.on_event("startup")
-async def startup_event():
-    scheduler.start()
+
